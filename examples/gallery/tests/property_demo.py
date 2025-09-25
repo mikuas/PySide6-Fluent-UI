26fe2dd608e05d7b471a117f1360c00a3a4524eb
Mikuas <Ciallo@0d00.cn>
2025-09-25 23:31:55 +0800

    .

--- examples/gallery/tests/property_demo.py
+++ examples/gallery/tests/property_demo.py
@@ -1,1 +1,76 @@
 # coding:utf-8
+import random
+import sys
+
+
+class Person:
+
+    def __init__(self):
+        super().__init__()
+        self._name: str = "TEST"
+        self._age: int = 0
+
+    @property
+    def name(self) -> str:
+        print("name getter")
+
+        return self._name
+
+    @name.setter
+    def name(self, name: str) -> None:
+        print("name setter")
+        if name.strip() == "":
+            raise "Names cannot be empty"
+
+        self._name = name
+
+    @name.deleter
+    def name(self) -> None:
+        print("name deleter")
+        del self._name
+
+    def __getAge(self) -> int:
+        print("age getter")
+        return self._age
+
+    def __setAge(self, value: int) -> None:
+        print("age setter")
+        if value < 0:
+            raise "age lessThan zero"
+
+        self._age = value
+
+    def __deleteAge(self) -> None:
+        print("delete age")
+
+        del self._age
+
+    age = property(__getAge, __setAge, __deleteAge, "this is age property")
+
+def personDemo():
+    person: Person = Person()
+    person.name = "zhangsan"
+    print(person.name)
+
+    del person.name
+    try:
+        print(person.name)
+    except Exception:
+        print("CONTINUE")
+
+    person.age = 114
+    print(person.age)
+
+    del person.age
+    try:
+        print(person.age)
+    except Exception:
+        ...
+
+
+def main():
+    personDemo()
+
+
+if __name__ == '__main__':
+    main()
---
+++ examples/gallery/tests/qt_property_demo.py
@@ -0,0 +1,85 @@
+# coding:utf-8
+import sys
+
+from PySide6.QtCore import QObject, Property, Signal, QTimer, QPropertyAnimation, Qt
+from PySide6.QtGui import QPainter, QColor
+from PySide6.QtWidgets import QWidget
+
+
+class QtProperty(QObject):
+
+    valueChanged = Signal(int)
+
+    def __init__(self):
+        super().__init__()
+        self._value: int = 0
+
+        self.valueChanged.connect(lambda: print(f"{self.value = }"))
+
+    def getValue(self) -> int:
+        return self._value
+
+    def setValue(self, value: int) -> None:
+        if value < 0 or value == self._value:
+            return
+        self._value = value
+        self.valueChanged.emit(value)
+
+    value = Property(int, getValue, setValue)
+
+
+class Window(QWidget):
+    def __init__(self, parent: QWidget = None):
+        super().__init__(parent)
+        self.setFixedHeight(25)
+        self.setWindowFlags(Qt.WindowType.FramelessWindowHint | Qt.WindowType.WindowStaysOnTopHint)
+        self.setAttribute(Qt.WidgetAttribute.WA_TranslucentBackground)
+
+        self._property = None
+        self._position = None
+
+    def mousePressEvent(self, event):
+        super().mousePressEvent(event)
+        self._position = event.globalPosition().toPoint() - self.frameGeometry().topLeft()
+
+    def mouseMoveEvent(self, event):
+        super().mouseMoveEvent(event)
+        self.move(event.globalPosition().toPoint() - self._position)
+
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        painter.setRenderHint(QPainter.RenderHint.Antialiasing)
+        painter.setPen(QColor(0, 0, 0, 32))
+        painter.setBrush(QColor("deeppink"))
+        painter.drawRoundedRect(1, 1, self._property.value - 1, 24, 8, 8)
+
+
+def qtProperDemo():
+    from PySide6.QtWidgets import QApplication
+    app = QApplication(sys.argv)
+    qtProperty = QtProperty()
+    qtProperty.value = 10
+
+    animation = QPropertyAnimation(qtProperty, b"value")
+    animation.setStartValue(0)
+    animation.setEndValue(1000)
+    animation.setDuration(1000)
+
+    window = Window()
+    window._property = qtProperty
+    window.setFixedWidth(animation.endValue() + 2)
+    window.show()
+    qtProperty.valueChanged.connect(window.update)
+
+    animation.start()
+
+    sys.exit(app.exec())
+
+
+def main():
+    qtProperDemo()
+
+
+if __name__ == '__main__':
+    main()
+
